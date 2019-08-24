eaea81da7898c9d05967e79dc5d045e9053b900a
Lucas Leonardo <lucasleonardo@protonmail.com>
2019-08-24 17:41:15 -0300

    Agregado unit testing

---
+++ stego_test.py
@@ -0,0 +1,62 @@
+import base64
+import bitarray
+import random
+import unittest
+
+import stego
+
+PICS = [
+    'wall.png',
+    'wall2.png',
+    'wall3.png',
+    'thewall.png',
+]
+
+
+class TestStego(unittest.TestCase):
+    def test_set_pixel(self):
+        msg = "test"
+        color = 0
+        i = 0
+        k = 1
+
+        encoded_msg = base64.b64decode(msg.encode('ascii'))
+        bit_array = bitarray.bitarray()
+        bit_array.frombytes(encoded_msg)
+        bit_array = [int(b) for b in bit_array]
+
+        self.assertEqual(stego.set_pixel(color, bit_array, i, k), 1,
+                         "\ncolor: {}, bit to set: {}, k: {}".
+                         format(color, bit_array[i], k))
+
+        i = 1
+        self.assertEqual(stego.set_pixel(color, bit_array, i, k), 0,
+                         "\ncolor: {}, bit to set: {}, k: {}".
+                         format(color, bit_array[i], k))
+
+    def test_message_encode(self):
+        long_msg = "long boi" * 100000
+
+        for pic in PICS:
+            # Expected K
+            self.assertEqual(stego.message_encode(pic, 1, "nice"), 1)
+            self.assertEqual(stego.message_encode(pic, 2, "nice"), 1)
+            self.assertEqual(stego.message_encode(pic, 3, "nice"), 1)
+
+            # Big messages
+            self.assertEqual(stego.message_encode(pic, 1, long_msg), 2)
+            self.assertEqual(stego.message_encode(pic, 2, long_msg), 2)
+            self.assertEqual(stego.message_encode(pic, 3, long_msg), 2)
+
+    def test_message_decode(self):
+        msgs = ["test", "steganography is cool and fun!", "", "uwu"]
+
+        for pic in PICS:
+            for msg in msgs:
+                k = stego.message_encode(pic, 1, msg)
+                self.assertEqual(stego.message_decode(stego.lsb_pic(pic), k),
+                                 msg, "\nk: {}, image: {}".format(k, pic))
+
+
+if __name__ == '__main__':
+    unittest.main()
