882adee0e9c19d11e1c0b6541d139d05fb35458b
Lucas Leonardo <lucasleonardo@protonmail.com>
2019-09-02 19:59:49 -0300

    Cambiadas listas a diccionarios.
    Co-authored-by: Karen Palacio <karen.palacio.1994@gmail.com>

--- estimators.py
+++ estimators.py
@@ -10,9 +10,11 @@ import sys
 import stego
 
 SUCCESSFUL_DECODES = 0
-MSE = []
-SSIM = []
-PSNR = []
+MSE = dict()
+SSIM = dict()
+PSNR = dict()
+ENCODE_TIMES = dict()
+DECODE_TIMES = dict()
 
 
 def image_measurements(img, img_lsb):
@@ -29,17 +31,19 @@ def image_measurements(img, img_lsb):
 
 
 def image_analysis_graph(img_name, k, msg):
-    global SUCCESSFUL_DECODES
+    global SUCCESSFUL_DECODES, ENCODE_TIMES, DECODE_TIMES
 
     img_lsb_name = stego.lsb_pic(img_name)
 
     encode_time = time.clock()
     used_k = stego.message_encode(img_name, k, msg)
     encode_time = time.clock() - encode_time
+    ENCODE_TIMES[msg].append(encode_time)
 
     decode_time = time.clock()
     decoded_msg = stego.message_decode(img_lsb_name, used_k)
     decode_time = time.clock() - decode_time
+    DECODE_TIMES[msg].append(decode_time)
 
     if decoded_msg == msg:
         SUCCESSFUL_DECODES += 1
@@ -56,9 +60,9 @@ def image_analysis_graph(img_name, k, msg):
 
     mse, ssim, psnr = image_measurements(img_name, img_lsb_name)
     global MSE, SSIM, PSNR
-    MSE.append(mse)
-    SSIM.append(ssim)
-    PSNR.append(psnr)
+    MSE[msg].append(mse)
+    SSIM[msg].append(ssim)
+    PSNR[msg].append(psnr)
 
     msg = '\n'.join(wrap(msg, 50))
     if len(msg) > 100:
@@ -90,48 +94,94 @@ def image_analysis_graph(img_name, k, msg):
 
 PICS = [
     'img/N1.png',
-    'img/N2.png',
-    'img/N3.png',
-    'img/N4.png',
-    'img/P1.jpg',
-    'img/P2.jpg',
-    'img/P3.jpg',
-    'img/P4.png',
-    'img/S1.jpg',
-    'img/S2.png',
-    'img/T1.png',
-    'img/T2.png',
-    'img/T3.png',
+    # 'img/N2.png',
+    # 'img/N3.png',
+    # 'img/N4.png',
+    # 'img/P1.jpg',
+    # 'img/P2.jpg',
+    # 'img/P3.jpg',
+    # 'img/P4.png',
+    # 'img/S1.jpg',
+    # 'img/S2.png',
+    # 'img/T1.png',
+    # 'img/T2.png',
+    # 'img/T3.png',
     'img/T4.png',
 ]
 
-i = 0
+MESSAGES = [
+    "a",
+    "Paris",
+    "steganography is cool and fun",
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." * 100,
+]
+
+COLORS = [
+    "tab:blue",
+    "tab:purple",
+    "tab:green",
+    "tab:red",
+]
+
+# Init dicts
+for msg in MESSAGES:
+    MSE[msg] = []
+    SSIM[msg] = []
+    PSNR[msg] = []
+    ENCODE_TIMES[msg] = []
+    DECODE_TIMES[msg] = []
+
+# Do the important work
+img_count = 0
 for img in PICS:
     for k in range(1, 4):
-        image_analysis_graph(img, k, "a")
-        i += 1
-        image_analysis_graph(img, k, "Paris")
-        i += 1
-        image_analysis_graph(img, k, "steganography is cool and fun")
-        i += 1
-        image_analysis_graph(img, k, "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." * 100)
-        i += 1
+        for msg in MESSAGES:
+            image_analysis_graph(img, k, msg)
+            img_count += 1
 
-print("Successfully decoded", SUCCESSFUL_DECODES, "out of", i, "images")
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(10, 4),
+print("Successfully decoded", SUCCESSFUL_DECODES, "out of", img_count, "images")
+
+# Measurements
+for j, msg in enumerate(MESSAGES):
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 6),
+                             sharex=True, sharey=False, constrained_layout=True)
+    axes[0].set_title("Median Square Error")
+    axes[0].set_ylabel("MSE values")
+
+    axes[1].set_title("Structural Similarity Index")
+    axes[1].set_ylabel("SSIM values")
+
+    axes[2].set_title("Peak Signal-to-Noise Ratio")
+    axes[2].set_ylabel("PSNR values")
+
+    for i, dic in enumerate([MSE, SSIM, PSNR]):
+        axes[i].plot(range(1, len(dic[msg]) + 1), dic[msg], 'o',
+                     c=COLORS[j], alpha=0.5)
+        axes[i].ticklabel_format(useOffset=False)
+
+    # Cut the long message
+    if j == 3:
+        msg = msg[:15] + "[..]"
+
+    fig.suptitle(msg)
+    plt.savefig("measures" + str(j) + ".png")
+    plt.close('all')
+
+# Time
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4),
                          sharex=True, sharey=False)
-axes[0].set_title("Median Square Error")
-axes[0].set_ylabel("MSE values")
-axes[0].scatter([x for x in range(0, i)], MSE)
+axes[0].set_title("Encoding Time")
+axes[0].set_ylabel("Secods")
 
-axes[1].set_title("Structural Similarity Index")
-axes[1].set_ylabel("SSIM values")
-axes[1].scatter([x for x in range(0, i)], SSIM)
+axes[1].set_title("Decoding Time")
+axes[1].set_ylabel("Seconds")
 
-axes[2].set_title("Peak Signal-to-Noise Ratio")
-axes[2].set_ylabel("PSNR values")
-axes[2].scatter([x for x in range(0, i)], PSNR)
+for j, msg in enumerate(MESSAGES):
+    axes[0].plot(total_img,
+                 ENCODE_TIMES[msg], c=COLORS[j], alpha=0.5)
+    axes[1].plot(total_img,
+                 DECODE_TIMES[msg], c=COLORS[j], alpha=0.5)
 
 plt.tight_layout()
-plt.savefig("measures.png")
+plt.savefig("time.png")
 plt.close('all')
